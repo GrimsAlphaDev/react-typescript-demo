0ee49ca7eff872d66ca218177a49307f575f79c1
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-09-10 12:04:45 +0700

    basic props

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Greet } from './components/greet';
+import { Person } from './components/person';
+import { PersonList } from './components/personList';
 
 function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne'
+  }
+
+  const nameList = [{
+    first: 'Bruce',
+    last: 'Wayne'
+  }, {
+    first: 'Clark',
+    last: 'Kent'
+  }, {
+    first: 'Diana',
+    last: 'Princes'
+  }]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name="Jonas Blue" messageCount={21} isLoggedIn={true} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
