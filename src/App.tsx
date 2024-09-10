b939d3b42c91d53c052ede3915862b92d0349e52
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-09-10 12:34:52 +0700

    event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import './App.css';
-import { Greet } from './components/greet';
-import { Heading } from './components/Heading';
-import { Oscar } from './components/Oscar';
-import { Status } from './components/Status';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
 
 function App() {
 
   return (
     <div className="App">
-      <Status status='loading' />
-      <Heading>Placeholder Text</Heading>
-      <Oscar>
-        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
-      </Oscar>
-      <Greet name='Batman' isLoggedIn={false}/>
+      <Button handleClick={(event, id) => {
+        console.log('Button clicked', event, id);
+      }} />
+      <Input value="" handleChange={(event) => console.log(event)} />
     </div>
   );
 }
