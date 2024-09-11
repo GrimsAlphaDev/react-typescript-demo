acacbedb70a28b0aeda6e53b79e5029b77561602
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-09-11 19:54:54 +0700

    useState Future Value

---
+++ src/components/state/User.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+type AuthUser = {
+    name: string;
+    email: string;
+}
+
+export const User = () => {
+    const [User, setUser] = useState<AuthUser|null>(null);
+    const handleLogin = () => {
+        setUser({
+            name: 'Taro',
+            email: 'test@gmail.com'
+        });
+    }
+    const handleLogout = () => {
+        setUser(null)
+    }
+
+    return (
+        <div>
+            <button onClick={handleLogin}>Login</button>
+            <button onClick={handleLogout}>Logout</button>
+            <div>User name is {User?.name}</div>
+            <div>User email is {User?.email}</div>
+        </div>
+    )
+}
